47e35fd836fd3793ae6adf798e291199f2ff1d58
Ilya Oberemok <namakete.dev@gmail.com>
2022-02-26 07:45:55 +0300

    upd

--- ex.1/src/main.f90
+++ ex.1/src/main.f90
@@ -1,6 +1,22 @@
 program main
-   print "('Hello World!')"
+  implicit none
+  real(8) :: a,b,c,qwe
+  real(8),external :: matrixDeterminanta
+  a = 2
+  b = 4
+  c = 6
+  qwe = matrixDeterminanta(a,b,c)
+
+  print *, qwe
+
 end program main
+
+real(8) function matrixDeterminanta(num1, num2, num3) result(answer) 
+  real(8) :: num1, num2, num3
+  answer = num1 + num2 + num3
+end function matrixDeterminanta
+
+
 
 
 
