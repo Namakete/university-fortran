e2c395344345f41a8bd29627a9aed25154a644b5
Ilya Oberemok <namakete.dev@gmail.com>
2022-02-28 20:35:39 +0300

    upd

--- ex.1/src/main.f90
+++ ex.1/src/main.f90
@@ -1,11 +1,20 @@
 program main
-  use sub
+
+  use iso_fortran_env
 
   implicit none
 
-  integer :: a = 7, b = 7
+  real(8) :: a = 2, b = 4, c = 6, d = 8, e = 10, f = 12
 
-  print *, plus(a, b)
+  real(8) :: f1, f2
+
+  f1 = (c * e - f * b) / (a * e - d * b)
+  f2 = (a * f - d * c) / (a * e - d * b) 
+
+  print *, f1
+  print *, f2
+
+
 
 end program main
 
