f2c4f2c35434ffd56fdd7b56eb34796d189f08b8
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-02 09:31:18 +0300

    upd

--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -4,11 +4,24 @@ program main
 
    implicit none
 
-   
+   character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
+   character(:), allocatable  :: fmt
+   integer                    :: In = 0, Out = 1
+   real(R_)                   :: a, b, c, d, x, w
 
-   
+   open (file=input_file, newunit=In)
+      read (In, *) a, b, c, d, x, w
+   close(In)
 
 
+
+
+
+   open (file=output_file, encoding=E_, newunit=Out)
+      fmt = "(a, T7, '= ', f6.2)"
+      write (Out, fmt) "y1"
+      write (Out, fmt) "y2"
+    close (Out)        
 
 end program main
 
