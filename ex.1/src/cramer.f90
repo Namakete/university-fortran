bf957eff44d265e71ab32fd47975c284d8890518
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-02 08:02:31 +0300

    upd

--- ex.1/src/cramer.f90
+++ ex.1/src/cramer.f90
@@ -1,23 +1,25 @@
 module cramer
 
+    use environment
+
     implicit none
 
     public :: f1, f2
 
     contains
 
-    integer function denominator(a,b,d,e) result(answer)
-        integer :: a,b,d,e
+    real function denominator(a,b,d,e) result(answer)
+        real :: a,b,d,e
         answer = (a * e - d * b)
     end function denominator    
 
-    integer function f1(b,c,e,f) result(answer)
-        integer :: b,c,e,f
+    real function f1(b,c,e,f) result(answer)
+        real :: b,c,e,f
         answer = (c * e - f * b)
     end function f1
 
-    integer function f2(a,c,d,f) result(answer)
-        integer :: a,c,d,f
+    real function f2(a,c,d,f) result(answer)
+        real :: a,c,d,f
         answer = (a * f - d * c) 
     end function f2
 
--- ex.1/src/main.f90
+++ ex.1/src/main.f90
@@ -1,20 +1,29 @@
 program main
-
+  use environment
   use cramer
 
   implicit none
 
-  integer :: a = 2, b = 4, c = 6, d = 8, e = 10, f = 12
-  integer :: m1, m2 = 2
+  character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
+  character(:), allocatable  :: fmt
+  integer                    :: In = 0, Out = 1
+  real(R_)                   :: a, b, c, d, e, f, m1, m2
+
+  open (file=input_file, newunit=In)
+    read (In, *) a, b, c, d, e, f
+  close(In)
 
   if (denominator(a,b,d,e) == 0) then
     print *, 'System determinant is zero'
   else
     m1 = f1(b,c,e,f) / denominator(a,b,d,e)
     m2 = f2(a,c,d,f) / denominator(a,b,d,e)
-
-    print *, 'f1 = ',m1
-    print *, 'f2 = ',m2
+    
+    open (file=output_file, encoding=E_, newunit=Out)
+      fmt = "(a, T7, '= ', f6.2)"
+      write (Out, fmt) "f1", m1
+      write (Out, fmt) "f2", m2
+    close (Out)
   end if
 
 end program main
