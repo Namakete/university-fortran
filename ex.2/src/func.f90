c67b2c10a0a1a4be752bd54790ce4dadaccf4727
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-02 10:42:30 +0300

    upd

--- ex.2/src/func.f90
+++ ex.2/src/func.f90
@@ -1,16 +1,36 @@
 module func
-
     use environment
 
     implicit none
 
-    public :: f
+    public :: shorthand_multiplication ,digit_conditions, calc
 
     contains
 
-    real function f(a, b) result(answer)
+    real function calc(x, w, op) result(r)
+        real :: x, w
+        character :: op
+        
+        select case (op)
+        case('+')
+            r = x**2 + w**2
+        case('-')
+            r = x**2 - w**w
+        end select
+    end function calc
+
+    real function digit_conditions(num_firts, num_secont, digit) result(r)
+        real :: num_firts, num_secont, digit
+        if(digit >= num_firts .and. digit <= num_secont) then
+            r = digit
+        else 
+            write(*,*) "'digit' could be >= 'a' or <= 'b'" 
+        end if 
+    end function digit_conditions
+
+    real function shorthand_multiplication(a, b) result(r)
         real :: a, b
-        answer = (a + b) / 2
-     end function f
+        r = (a + b) / 2
+    end function shorthand_multiplication
 
 end module func
--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -7,20 +7,23 @@ program main
    character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
    character(:), allocatable  :: fmt
    integer                    :: In = 0, Out = 1
-   real(R_)                   :: a, b, c, d, x, w
+   real(R_)                   :: a, b, c, d, x, w, y1!, y2
+   character                  :: plus = '+'!, minus = '-'
 
    open (file=input_file, newunit=In)
       read (In, *) a, b, c, d, x, w
    close(In)
 
-
+   !if(digit_conditions(a, b, x) < shorthand_multiplication(a, b)) then
+      y1 = calc(x,w, plus)
+   !end if
 
 
 
    open (file=output_file, encoding=E_, newunit=Out)
       fmt = "(a, T7, '= ', f6.2)"
-      write (Out, fmt) "y1"
-      write (Out, fmt) "y2"
+      write (Out, fmt) "y1", y1
+      !write (Out, fmt) "y2"
     close (Out)        
 
 end program main
