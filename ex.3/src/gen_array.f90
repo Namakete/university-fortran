f1d117ef94ce3ac35952b4a031a8ab7b66786d4e
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-04 17:20:15 +0300

    upd

---
+++ ex.3/src/gen_array.f90
@@ -0,0 +1,25 @@
+module gen_array
+    use environment
+
+    implicit none
+
+    public::number_random_generation
+
+contains
+
+subroutine number_random_generation(n)
+    integer :: n, Out = 0, i
+    character(*), parameter    :: input_file = "../data/input.txt"
+    real(4) :: random
+
+    open(file=input_file, encoding=E_, newunit=Out)
+    write(Out, "(i10)") n
+    do i = 1, n
+        call random_seed()
+        call random_number(random)
+        write (Out, "(f6.3)") random
+    end do
+    close(Out)
+end subroutine number_random_generation
+
+end module gen_array
--- ex.3/src/main.f90
+++ ex.3/src/main.f90
@@ -1,22 +1,28 @@
 program main
   use environment
+  use gen_array
 
   implicit none
   
 character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
   character(:), allocatable  :: fmt
-  integer                    :: In = 0, Out = 1
-  real(R_)                   :: a, b, z
+  integer                    :: In = 0, Out = 1, arr_size = 10, n = 0
+  real(R_), allocatable      :: Arr(:)
 
+  call number_random_generation(arr_size)
+  
   open (file=input_file, newunit=In)
-  read (In, *) a, b
+    read (In, *) n
+    allocate(Arr(n))
+    read(In, *) Arr
   close(In)
 
-  z = a + b
+  
+
 
   open (file = output_file, encoding = E_, newunit = Out)
-  fmt = "(a, T7, '= ', f6.2)"
-  write (Out, fmt) "z=", z
+    fmt = "(a, T7, '= ', f6.2)"
+    write (Out, fmt) "z="
   close (Out)
   
 end program main
