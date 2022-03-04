7eca0644e4c08aeba9adc20d272f9b9e74ef6b13
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-04 16:25:59 +0300

    upd

--- ex.2/src/func.f90
+++ ex.2/src/func.f90
@@ -1,36 +1,27 @@
 module func
-    use environment
+  use environment
 
-    implicit none
+  implicit none
 
-    public :: shorthand_multiplication, digit_conditions, calculation
+  public :: shorthand_multiplication, calculation
 
-    contains
+contains
 
-    real function calculation(x, w, op) result(r)
-        real :: x, w
-        character :: op
-        
-        select case (op)
-        case('+')
-            r = x**2 + w**2
-        case('-')
-            r = x**2 - w**w
-        end select
-    end function calculation
+  real function calculation(x, w, op) result(r)
+    real :: x, w
+    character :: op
 
-    real function digit_conditions(num_firts, num_secont, digit) result(r)
-        real :: num_firts, num_secont, digit
-        if(digit >= num_firts .and. digit <= num_secont) then
-            r = digit
-        else 
-            write(*,*) "'digit' could be >= or <= " 
-        end if 
-    end function digit_conditions
+    select case (op)
+    case('+')
+      r = x**2 + w**2
+    case('-')
+      r = x**2 - w**2
+    end select
+  end function calculation
 
-    real function shorthand_multiplication(a, b) result(r)
-        real :: a, b
-        r = (a + b) / 2
-    end function shorthand_multiplication
+  real function shorthand_multiplication(num1, num2) result(r)
+    real :: num1, num2
+    r = (num1 + num2) / 2
+  end function shorthand_multiplication
 
 end module func
--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -1,28 +1,34 @@
 program main
-   use func
-   use environment
+  use func
+  use environment
 
-   implicit none
+  implicit none
 
-   character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-   character(:), allocatable  :: fmt
-   integer                    :: In = 0, Out = 1
-   real(R_)                   :: a, b, c, d, x, w, y1, y2
-   character                  :: plus = '+', minus = '-'
+  character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
+  character(:), allocatable  :: fmt
+  integer                    :: In = 0, Out = 1
+  real(R_)                   :: a, b, c, d, x, w
+  character                  :: plus = '+', minus = '-'
 
-   open (file=input_file, newunit=In)
-      read (In, *) a, b, c, d, x, w
-   close(In)
+  open (file=input_file, newunit=In)
+  read (In, *) a, b, c, d, x, w
+  close(In)
 
+  open (file=output_file, encoding=E_, newunit=Out)
+  fmt = "(a, T7, f6.2)"
 
-   
+  if ((a <= x .and. x <= b) .and. (c <= w .and. w <= d)) then
+    if((x < shorthand_multiplication(a, b)) .and. (w <= shorthand_multiplication(c, d))) then
+      write (Out, fmt) "y1=", calculation(x, w, plus)
+      write (Out, fmt) "y2=", calculation(x, w, minus)
+    else 
+      write (Out, fmt) "error1"
+    end if
+  else
+    write (Out, fmt) "error2"
+  end if
 
-   open (file=output_file, encoding=E_, newunit=Out)
-      fmt = "(a, T7, '= ', f6.2)"
-      !write (Out, fmt) "y1=", y1
-      !write (Out, fmt) "y2=", y2
-   close (Out) 
-  
+  close (Out)
 
 end program main
 
