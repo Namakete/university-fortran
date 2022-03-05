a93c2c783bc859da8127dc4f6fac6c20f5649d64
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-05 05:09:26 +0300

    upd

--- ex.3/src/main.f90
+++ ex.3/src/main.f90
@@ -2,27 +2,26 @@ program main
   use environment
 
   implicit none
-  
-   character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-   integer                    :: In = 0, Out = 0, rows = 0, columns = 0!, i = 0
-   integer, allocatable       :: A(:,:)
-   integer                    :: res_rows = 0, res_columns = 0 
-         
-   open (file=input_file, newunit=In)
-    read(In, *) rows, columns
-    allocate(A(rows, columns))
-    read (In, *) A
-   close (In)
 
-   res_rows = sum(A(1:columns+1,1), dim=1)
+  character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
+  integer                    :: In = 0, Out = 0,  rows = 0, columns = 0, i = 0
+  integer, allocatable       :: A(:,:)
 
-   res_columns = sum(A(1:rows+1,1), dim=1)
+  open (file=input_file, newunit=In)
+   read(In, *) rows, columns
 
-   !outout data
-   open (file=output_file, encoding=E_, newunit=Out, position='append')
-      write(*,*)"rows:",res_rows
-      write(*,*)"columns:",res_columns
-   close (Out)
+   allocate(A(rows, columns))
+
+   do i = 1, rows
+      read(in,*) a(i,:)
+   end do
+  close (In)
+
+  open (file=output_file, newunit=Out)
+      print '(A,4(1X,I0))', 'row:', sum(a,dim=2)
+      print '(A,4(1X,I0))', 'column:',  sum(a,dim=1)
+  close (Out)
+
 
 end program main
 
