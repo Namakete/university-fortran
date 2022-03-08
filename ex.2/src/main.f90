6250cbcedab38305d9231707f6f67cf49d3daeb2
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-08 14:30:25 +0300

    upd

--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -7,7 +7,6 @@ program main
     character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
     integer                    :: In = 0, Out = 0
     real(R_)                   :: a = 0, b = 0, c = 0, d = 0, x = 0, w = 0, func = 0
-    Character(len = 20) :: error
 
     open (file=input_file, newunit=In)
         read (In, *) a, b, c, d, x, w
@@ -17,33 +16,32 @@ program main
       write (Out, "(6(a, f0.2/))") "a = ", a, "b = ", b, "c = ", c, "d = ", d, "x = ", x, "w = ", w
    close (Out)
 
-   if ((a <= x .and. x <= b) .and. (c <= w .and. w <= d)) then
-        func = F(a, b, x, w)
-   else 
-
-   end if
-
-    
-    open (file=output_file, encoding=E_, newunit=Out, position='append')
-        write(Out, "(1(a, f0.2/))") "f1 = ", func, error
-    close (Out)
-
+   func = F(a, b, c, d, x, w)
+  
 contains
 
-    pure function F(a, b, x, w)
-        real(R_) F, a, b, x, w
-        intent(in) a, b, x, w
-
-        if(x < ((a + b)/2)) then
-            F = (x**2 + w**2)
-        else if (w <= ((a + b)/2)) then
-            F = (x**2 - w**2)
-        else 
-            F = IEEE_Value(x, IEEE_Quiet_NaN)
-        end if
-        
+    function F(a, b, c, d, x, w)
+        real(R_) F, a, b, c, d, x, w
+        intent(in) a, b, c, d, x, w
+ 
+        open (file=output_file, encoding=E_, newunit=Out, position='append')
+        eval: block
+            if ((a <= x .and. x <= b) .and. (c <= w .and. w <= d)) then
+                if(x < ((a + b)/2)) then
+                    F = (x**2 + w**2)
+                else if (w <= ((a + b)/2)) then
+                    F = (x**2 - w**2)
+                else 
+                    F = IEEE_Value(x, IEEE_Quiet_NaN)
+                end if
+                write (Out, "('y = ', f0.2)") F
+            else
+                write (Out, "('f is indetermined')")
+            end if
+        end block eval
+        close (Out)
     end function F
-
+    
 end program main
 
 
