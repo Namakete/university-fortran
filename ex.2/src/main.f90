c938ffe72b2d992c0e90e1562a9f0f78019a6214
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-08 12:24:51 +0300

    upd

--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -1,42 +1,46 @@
 program main
     use environment
+    use IEEE_Arithmetic
 
     implicit none
 
     character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-    character(:), allocatable  :: fmt
-    integer                    :: In = 0, Out = 1
-    real(R_)                   :: a, b, c, d, x, w, y1, y2
+    integer                    :: In = 0, Out = 0
+    real(R_)                   :: a = 0, b = 0, c = 0, d = 0, x = 0, w = 0, func = 0
 
     open (file=input_file, newunit=In)
         read (In, *) a, b, c, d, x, w
     close(In)
 
-    
+    open (file=output_file, encoding=E_, newunit=Out)
+      write (Out, "(4(a, f0.2/))") "a = ", a, "b = ", b, "c = ", c, "d = ", d, "x = ", x, "w = ", w
+    close (Out)
+
     if ((a <= x .and. x <= b) .and. (c <= w .and. w <= d)) then
-        if(x < shorthand_multiplication(a, b)) then
-            y1 = (x**2 + w**2)
-        else if (w <= shorthand_multiplication(c, d)) then
-            y1 = (x**2 - w**2)
-        else 
-            write (Out, fmt) "Case not provided"
-        end if
-        else
-            write (Out, fmt) "Case not provided"
+        func = F(a, b, x, w)
+    else 
+        write(Out, "('f = ', f0.2)") "Error"
     end if
     
-    open (file=output_file, encoding=E_, newunit=Out)
-        fmt = "(a, T7, f6.2)"
-        write(Out, fmt) 'y1', y1
-        write(Out, fmt) 'y2', y2
+    open (file=output_file, encoding=E_, newunit=Out, position='append')
+        write(Out, "(1(a, f0.2/))") "f1 = ", func
     close (Out)
 
 contains
 
-    real function shorthand_multiplication(num1, num2) result(r)
-        real :: num1, num2
-        r = (num1 + num2) / 2
-    end function shorthand_multiplication
+    pure function F(a, b, x, w)
+        real(R_) F, a, b, x, w
+        intent(in) a, b, x, w
+
+        if(x < ((a + b)/2)) then
+            F = (x**2 + w**2)
+        else if (w <= ((a + b)/2)) then
+            F = (x**2 - w**2)
+        else 
+            F = IEEE_Value(x, IEEE_Quiet_NaN)
+        end if
+        
+    end function F
 
 end program main
 
