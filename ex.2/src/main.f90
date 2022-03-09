cbaf090368d2436b0dbbe19f7ff2fb2990e86dfc
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-09 17:23:40 +0300

    upd

--- ex.2/src/main.f90
+++ ex.2/src/main.f90
@@ -6,39 +6,42 @@ program main
 
     character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
     integer                    :: In = 0, Out = 0
-    real(R_)                   :: a = 0, b = 0, c = 0, d = 0, x = 0, w = 0, func = 0
+    real(R_)                   :: a = 0, b = 0, c = 0, d = 0, x = 0, w = 0, func = 0, dig = 0
+
 
     open (file=input_file, newunit=In)
         read (In, *) a, b, c, d, x, w
     close(In)
 
+    dig = ((a + b) / 2)
+
     open (file=output_file, encoding=E_, newunit=Out)
-      write (Out, "(6(a, f0.2/))") "a = ", a, "b = ", b, "c = ", c, "d = ", d, "x = ", x, "w = ", w
+      write (Out, "(4(a, f0.2/))") "a = ", a, "b = ", b, "c = ", c, "d = ", d
+      write (Out, "(2(a, f0.2/))") "x = ", x, "w = ", w
+      write (Out, "(1(a, f0.2/))") "dig = ", dig
    close (Out)
 
-   func = F(a, b, c, d, x, w)
-  
+   func = F(a, b, c, d, x, w, dig)
+
 contains
 
-    function F(a, b, c, d, x, w)
-        real(R_) F, a, b, c, d, x, w
-        intent(in) a, b, c, d, x, w
+    function F(a, b, c, d, x, w, dig)
+        real(R_) F, a, b, c, d, x, w, dig
+        intent(in) a, b, c, d, x, w, dig
  
         open (file=output_file, encoding=E_, newunit=Out, position='append')
-        eval: block
             if ((a <= x .and. x <= b) .and. (c <= w .and. w <= d)) then
-                if(x < ((a + b)/2)) then
+                if(x < dig) then
                     F = (x**2 + w**2)
-                else if (w <= ((a + b)/2)) then
+                else if (w <= dig) then
                     F = (x**2 - w**2)
                 else 
                     F = IEEE_Value(x, IEEE_Quiet_NaN)
                 end if
-                write (Out, "('y = ', f0.2)") F
+                write (Out, "('y(x,w) = ', f0.2)") F
             else
                 write (Out, "('Case not provided')")
             end if
-        end block eval
         close (Out)
     end function F
     
