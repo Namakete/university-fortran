404f0e8b85e8db0de185979ca28552cd35d37fae
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-10 11:39:15 +0300

    upd

--- ex.4/src/main.f90
+++ ex.4/src/main.f90
@@ -4,21 +4,40 @@ program main
   implicit none
   
 character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-  character(:), allocatable  :: fmt
-  integer                    :: In = 0, Out = 1
-  real(R_)                   :: a, b, z
+  integer                    :: In = 0, Out = 0
+  real(R_)                   :: a = 0, b = 0, n = 100, h = 0, dx = 0
 
   open (file=input_file, newunit=In)
-  read (In, *) a, b
+    read (In, *) a, b, h
   close(In)
+  
+  dx = (0.8 * x *)
 
-  z = a + b
+  h = trap_f(a, b, n)
+
+
+
+
+
+
+
 
   open (file = output_file, encoding = E_, newunit = Out)
-  fmt = "(a, T7, '= ', f6.2)"
-  write (Out, fmt) "z=", z
+    write (Out, *) "Trapez: "
   close (Out)
-  
+
+contains 
+
+  !Формлу трапеций, которая вычисляет длину каждого
+  !маленького отрезка или шага
+  pure function trap_f(a, b, n)
+    real(R_) trap_f, a, b, n
+    intent(in) a, b, n
+
+    trap_f = ((b - a) / n)
+
+  end function trap_f
+
 end program main
 
 
