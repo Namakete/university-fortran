758da86d2bf856ea76e5e1c4f0be1a165484279c
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-10 19:28:32 +0300

    upd

--- ex.6/src/main.f90
+++ ex.6/src/main.f90
@@ -4,19 +4,58 @@ program main
   implicit none
   
   character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-  integer                    :: In = 0, Out = 1
-  real(R_)                   :: a, b, z
+  integer                    :: In = 0, Out = 1, N = 0, Neg = 0, i = 0
+  real(R_), allocatable      :: A(:)
+  logical, allocatable       :: Mask(:)   
 
   open (file=input_file, newunit=In)
-  read (In, *) a, b
+    read (In, *) N
+    allocate(A(N))
+    read(In, *) A
   close(In)
 
-  
+  Mask = A <= 0
+
+  Neg = count(Mask)
+
+  A=[Pack(A, .not. Mask), Pack(A, Mask)]
+
+  do i = 1, N
+    if ( A(i)>0) then
+      call sort(A,i,N)
+      exit
+    end if
+ end do
 
   open (file = output_file, encoding = E_, newunit = Out)
-  write (Out, *)
+    write(Out, fmt = '(10F10.2)') A(:)
   close (Out)
   
+contains
+
+  recursive subroutine sort(a, first, last)
+    implicit none
+    real(R_)  a(*), x, t
+    integer first, last
+    integer i, j
+    x = a( (first+last) / 2 )
+    i = first
+    j = last
+    do
+      do while (a(i) < x)
+          i=i+1
+      end do
+      do while (x < a(j))
+          j=j-1
+      end do
+      if (i >= j) exit
+      t = a(i);  a(i) = a(j);  a(j) = t
+      i=i+1
+      j=j-1
+    end do
+    if (first < i-1) call quicksort(a, first, i-1)
+    if (j+1 < last)  call quicksort(a, j+1, last)
+  end subroutine sort  
 end program main
 
 
