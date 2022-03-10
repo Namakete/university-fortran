a834fc10d8aaa0c1299441a66de623b1e8b0bbec
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-10 13:49:06 +0300

    upd

--- ex.5/src/main.f90
+++ ex.5/src/main.f90
@@ -4,17 +4,44 @@ program main
   implicit none
   
 character(*), parameter      :: input_file = "../data/input.txt", output_file = "output.txt"
-  integer                    :: In = 0, Out = 0, M(25)
-  real(R_), allocatable      :: X(:)
+  integer                    :: In = 0, Out = 0, N = 0, M = 0
+  logical                    :: mask = .true.
+  integer, allocatable       :: X(:), Mat(:), Slice(:)
+  logical, allocatable       :: Neg(:)
 
+  open (file=input_file, newunit = In)
+    read (In, *) N
+    allocate(X(N))
+    read(in,*) X
+  close(In)
 
+  Mat = X
+  Slice = pack(Mat, Mask)
 
-  open (file = output_file, encoding = E_, newunit = Out)
+  allocate(Neg(N))
+  call Negative(Slice, Neg, M)
 
+  open (file=output_file, encoding=E_, newunit=Out)
+  write (Out, "("//N//"(i0, 1x))") X
+  write (Out, *) Neg
+  write (Out, '(/1(a, T11, ": ", i0))') 'Array Size', N
+  write (Out, '(1(a, T21, ": ", i0/))') 'Value negative items', M
   close (Out)
-  
-end program main
 
+contains
+
+  pure subroutine Negative(X, Neg, M)
+    integer     X(:), M
+    logical     Neg(:)
+    intent(in)  X
+    intent(Out) Neg, M
+
+    Neg = X < 0
+    M = Count(Neg)
+
+ end subroutine Negative
+ 
+end program main
 
 
 
