e9e712b7bdbfb1088445667dd8c36b16f6538852
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-12 15:51:07 +0300

    upd

--- ex.7/ex.7.16.a/src/main.f90
+++ ex.7/ex.7.16.a/src/main.f90
@@ -1,29 +1,41 @@
-program main
-  use environment
-
+program lab7_15a 
+  use Environment
   implicit none
+  character(*), parameter     :: input_file = "../data/input.txt" , output_file = "output.txt"
+  integer                     :: In = 0, Out = 0, rows = 0, columns = 0, i = 0
+  real(R_)                    :: Z = 0
+  real(R_), allocatable       :: A(:,:)
   
-character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-  integer                  :: In = 0, Out = 0, size
-  integer, allocatable    :: A(:,:)
-
   open (file=input_file, newunit=In)
-    read (In, *) size
-    allocate(A(size, size))
-    read (In, *) A
-  close(In)
+     read(In, *) rows, columns
+     allocate(A(rows, columns))                                                
+     read(In, *) (A(i,:), i = 1, rows)                                 
+  close (In) 
 
-  open (file = output_file, encoding = E_, newunit = Out)
-    write (Out, *) "MinVal", MinVal((A))
-    write (Out, *) "MaxVal", MaxVal((A))
-    write (Out, *) "MinLoc", MinLoc((A))
-    write (Out, *) "MaxLoc", MaxLoc((A))
-    write (Out, *) "Sum", Sum((A))
+  open (file=output_file, encoding=e_, newunit=out)
+     write (out,*)"Array"
+     write (out, "("//columns//"f6.1)") (A(i,:), i = 1, rows)  
+     close (out)
 
-  close (Out)
-  
-end program main
+  call MinMaxElement(A,Z) 
 
+  open (file=output_file, encoding=e_, newunit=out, position="append") 
+     write (Out, * ) 'Minimax element value by columns'          
+     write (Out,"(f6.1)") Z
+  close (out)
+
+contains 
+
+  pure subroutine MinMaxElement(A,Z)
+    real(R_) A(:,:)
+    real(R_) Z
+    intent(in) A 
+    intent(out) Z
+
+    Z = Minval(Maxval(A,dim=2))
+  end subroutine MinMaxElement
+
+end program lab7_15a 
 
 
 
