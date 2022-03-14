c60dff478d6e0d77824f62fbe7941e8bb14393ea
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-14 18:22:24 +0300

    upd

--- ex.7/ex.7.40/src/main.f90
+++ ex.7/ex.7.40/src/main.f90
@@ -3,20 +3,42 @@ program main
 
   implicit none
   
-character(*), parameter    :: input_file = "../data/input.txt", output_file = "output.txt"
-  character(:), allocatable  :: fmt
-  integer                    :: In = 0, Out = 1
-  real(R_)                   :: a, b, z
+character(*), parameter           :: input_file = "../data/input.txt", output_file = "output.txt"
+  integer                         :: In = 0, Out = 1, rows = 0, columns = 0, i = 0
+  integer, allocatable, target    :: A(:,:)
+  integer, contiguous, pointer    :: B(:)
+  logical, allocatable            :: Mask(:)
+  integer, allocatable            :: Indexes(:), MTIndexes(:), Elements(:)
+  
 
   open (file=input_file, newunit=In)
-  read (In, *) a, b
+    read (In, *) rows, columns
+    allocate (A(rows, columns))
+    read (In, *) (A(i, :), i = 1, rows)
   close(In)
 
-  z = a + b
+  open (file=output_file, encoding=E_, newunit=Out)
+      write (Out, '('//columns//'i4)') (A(i, :), i = 1, rows)
+  close (Out)
 
-  open (file = output_file, encoding = E_, newunit = Out)
-  fmt = "(a, T7, '= ', f6.2)"
-  write (Out, fmt) "z=", z
+  B(1:Size(A)) => A
+
+  Indexes = [(i, i = 1, Size(B))] 
+
+  Mask = (Mod(Indexes, 2) == 0)
+
+  MTIndexes = pack(B, mask)
+
+  print *, MTIndexes
+
+  do i = 1, Size(MTIndexes)
+    if (MTIndexes(i) < 1) Elements = MTIndexes(i)
+  end do
+
+  print *, Elements
+
+  open (file=output_file, encoding=E_, newunit=Out, position='append')
+    write (Out, *)
   close (Out)
   
 end program main
