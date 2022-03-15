296a308c7d5b741604d7f496d3e375dced132299
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-15 14:27:54 +0300

    upd

--- ex.7/ex.7.16.a/src/main.f90
+++ ex.7/ex.7.16.a/src/main.f90
@@ -1,41 +1,41 @@
-program lab7_15a 
-  use Environment
-  implicit none
-  character(*), parameter     :: input_file = "../data/input.txt" , output_file = "output.txt"
-  integer                     :: In = 0, Out = 0, rows = 0, columns = 0, i = 0
-  real(R_)                    :: Z = 0
-  real(R_), allocatable       :: A(:,:)
-  
-  open (file=input_file, newunit=In)
-     read(In, *) rows, columns
-     allocate(A(rows, columns))                                                
-     read(In, *) (A(i,:), i = 1, rows)                                 
-  close (In) 
+program lab7_15a
+    use Environment
+    implicit none
+    character(*), parameter     :: input_file = "../data/input.txt" , output_file = "output.txt"
+    integer                     :: In = 0, Out = 0, rows = 0, columns = 0, i = 0
+    real(R_)                    :: Z = 0
+    real(R_), allocatable       :: A(:,:)
 
-  open (file=output_file, encoding=e_, newunit=out)
-     write (out,*)"Array"
-     write (out, "("//columns//"f6.1)") (A(i,:), i = 1, rows)  
-     close (out)
+    open (file=input_file, newunit=In)
+        read(In, *) rows, columns
+        allocate(A(rows, columns))
+        read(In, *) (A(i,:), i = 1, rows)
+    close (In)
 
-  call MinMaxElement(A,Z) 
+    open (file=output_file, encoding=e_, newunit=out)
+        write (out,*)"Array"
+        write (out, "("//columns//"f6.1)") (A(i,:), i = 1, rows)
+    close (out)
 
-  open (file=output_file, encoding=e_, newunit=out, position="append") 
-     write (Out, * ) 'Minimax element value by columns'          
-     write (Out,"(f6.1)") Z
-  close (out)
+    call MinMaxElement(A,Z)
+    
+    open (file=output_file, encoding=e_, newunit=out, position="append")
+        write (Out, * ) 'Minimax element value by columns'
+        write (Out,"(f6.1)") Z
+    close (out)
 
-contains 
+contains
 
-  pure subroutine MinMaxElement(A,Z)
-    real(R_) A(:,:)
-    real(R_) Z
-    intent(in) A 
-    intent(out) Z
+    pure subroutine MinMaxElement(A,Z)
+        real(R_) A(:,:)
+        real(R_) Z
+        intent(in) A
+        intent(out) Z
 
-    Z = Minval(Maxval(A,dim=2))
-  end subroutine MinMaxElement
+        Z = Minval(Maxval(A, dim = 2))
+    end subroutine MinMaxElement
 
-end program lab7_15a 
+end program lab7_15a
 
 
 
--- ex.8/src/mxele.f90
+++ ex.8/src/mxele.f90
@@ -8,7 +8,7 @@ contains
         integer, intent(inout) :: Res
         integer  :: i
         
-        do i = 1, Size(B, dim = 1)
+        do i = 1, Size(B)
             if (B(i) > Res) Res = B(i)
         end do
     end subroutine max_ele_find
