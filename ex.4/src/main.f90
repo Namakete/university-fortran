1566dffa1395b1922561b48387a1b2d9dbcb6d5d
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-18 21:37:48 +0300

    upd

--- ex.4/src/main.f90
+++ ex.4/src/main.f90
@@ -3,31 +3,39 @@ program main
 
   implicit none
   character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
-  integer                 :: In = 0, Out = 0, i = 0, H = 0
-  real(R_)                :: a = 0, b = 0             
-  real(R_)                :: n                          
-  real(R_),allocatable    :: X(:)                    
-  real(R_)                :: Integral
+  integer                 :: In = 0, Out = 0, N = 0
+   real(R_)                :: a = 0, b = 0, h = 0, I = 0
+   real(R_), allocatable   :: X(:)                  
 
   open(newunit=In, file=input_file)
-     read(In, *) a, b, n
+     read(In, *) a, b, h
   close(In)
 
   open (file=output_file, encoding=E_, newunit=Out)
-      write (Out, '(3(a, T4, "= ", f0.4/))') "a", a, "b", b, "h", n
+      write (Out, '(3(a, T4, "= ", f0.4/))') "a", a, "b", b, "h", h
    close (Out)
 
-  H = Int(((b-a)/n+.5_R_)/2)
+  N = Int(((b-a)/h+.5_R_)/2)
 
-  allocate(X(H))
-
-  X = [((a/2)+2*(a+n*(i-1)), i = 1, H)]
-
-  Integral = n*Sum(.8_R_*(X)*(-exp(X**2+.5_R_)))
+  allocate(X(N))
+  call Integral(a, h, X, I)
 
   open (file=output_file, encoding=E_, newunit=Out, position='append')
-     write(Out,*) "The integral is", Integral
+     write(Out,*) "The integral is ", I
   close(Out)
+
+contains
+
+   pure subroutine Integral(a, h, X, I)
+      real(R_) a, h, X(:), I
+      intent(in) a, h
+      intent(out) X, I
+      integer j
+
+      X = [((a/2)+2*(a+h*(j-1)), j = 1, Size(X))]
+      X = .8_R_* X *(-exp(X**2+.5_R_))
+      I = Sum(X) * h
+   end subroutine Integral
 
 end program main
 
--- ex.7/ex.7.40/src/main.f90
+++ ex.7/ex.7.40/src/main.f90
@@ -5,25 +5,27 @@ program main
   
   character(*), parameter         :: input_file = "../data/input.txt", output_file = "output.txt"
   integer                         :: In = 0, Out = 1, rows = 0, columns = 0, i = 0
-  integer, allocatable, target    :: A(:,:)
+  integer, allocatable, target    :: C(:,:)
   integer, contiguous, pointer    :: B(:)
-  logical, allocatable            :: Mask(:)
+  logical, allocatable            :: Mask(:,:)
   integer, allocatable            :: Indexes(:), MTIndexes(:), MTElements(:) 
 
   open (file=input_file, newunit=In)
     read (In, *) rows, columns
-    allocate (A(rows, columns))
-    read (In, *) (A(i, :), i = 1, rows)
+    allocate (C(rows, columns))
+    read (In, *) (C(i, :), i = 1, rows)
   close(In)
 
   open (file=output_file, encoding=E_, newunit=Out)
-      write (Out, '('//columns//'i4)') (A(i, :), i = 1, rows)
+      write (Out, '('//columns//'i4)') (C(i, :), i = 1, rows)
   close (Out)
+    
+    allocate(Mask(rows, columns), source = .false.)
 
-  B(1:Size(A)) => A
+  B(1:Size(C)) => C
 
   Indexes = [(i, i = 1, Size(B))] 
-
+!
   Mask = ((Mod(Indexes, 2) == 0) .and. (B < 1))
 
   MTIndexes = pack(Indexes, mask)
