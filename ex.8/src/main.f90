7b2a0b15be44e83061beec2b10738f69283abbf0
Ilya Oberemok <namakete.dev@gmail.com>
2022-03-27 17:46:17 +0300

    upd

--- ex.8/src/main.f90
+++ ex.8/src/main.f90
@@ -8,7 +8,7 @@ program main
     character(*), parameter         :: input_file = "../data/input.txt"
     integer                         :: In = 0, Out = 0, rows = 0, columns = 0, i = 0, Max = 0, Min = 0
     integer, allocatable, target    :: A(:,:)
-    integer, contiguous, pointer    :: B(:), C(:), D(:)
+    integer, contiguous, pointer    :: B(:)
 
     Open (file=input_file, newunit=In)
         read (In, *) rows, columns
@@ -17,12 +17,11 @@ program main
     close (In)
 
     open (file=output_file, encoding=E_, newunit=Out)
-        write (Out, '('//columns//'i4)') (A(i, :), i = 1, rows)
+        write (Out, '('//rows//'i4)') (A(i, :), i = 1, rows)
     close (Out)
 
     B(1:Size(A)) => A
-    C(1:Size(A)) => A
-
+    
     call fina_max_min_elements(B, Max, Min)
 
     open (file=output_file, encoding=E_, newunit=Out, position='append')
