6485a1fd112a9ea5d47fb7eed52f1011338a1d84
Ilya Oberemok <namakete.dev@gmail.com>
2022-04-20 00:09:08 +0300

    Added new projects

---
+++ alg.ex.1.1.1/src/main.f90
@@ -0,0 +1,13 @@
+! Copyright (c) Namakete (Ilya Oberemok) <namakete.dev@gmail.com>.
+! See the LICENCE file in the repository root for full licence text.
+program main
+    use environment
+    implicit none
+
+    character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
+    integer                 :: Out = 0, In = 0
+
+    print *, "Hello World!"
+end program main
+
+
