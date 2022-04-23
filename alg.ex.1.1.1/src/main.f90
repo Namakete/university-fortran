9b07d7ec35f418085dd8443549df5766cbbd2b6d
Ilya Oberemok <namakete.dev@gmail.com>
2022-04-23 11:57:03 +0300

    Test lazyGit commit

--- alg.ex.1.1.1/src/main.f90
+++ alg.ex.1.1.1/src/main.f90
@@ -2,20 +2,12 @@
 ! See the LICENCE file in the repository root for full licence text.
 program main
     use environment
-<<<<<<< Updated upstream
     implicit none
 
     character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
     integer                 :: Out = 0, In = 0
 
-=======
-    
-    implicit none
-
-    character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
-    integer                 :: Out = 0, In = 0    
->>>>>>> Stashed changes
-    print *, "Hello World!"
+    print *, "Hellow"
 end program main
 
 
