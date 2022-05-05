a2533ef2af389ac034a1bfaa4b5960623c92ccbb
Ilya Oberemok <namakete.dev@gmail.com>
2022-05-05 18:52:32 +0300

    Fixed array variable names

--- alg.ex.1.3.1/src/group_process.f90
+++ alg.ex.1.3.1/src/group_process.f90
@@ -7,14 +7,14 @@ module Group_Process
    implicit none
    
 contains
-    pure function Sort_class_list(Group) result (Max_Amount_Index)
+    pure function Sort_class_list(Group) result (Max_Amount_Position)
         type(student), intent(in)   :: Group(:)
         real(R_)                    :: Max_Amount
-        integer, allocatable:: Max_Amount_Index(:)
+        integer, allocatable:: Max_Amount_Position(:)
         integer                     :: i
     
         Max_Amount = MaxVal(Real(Group%Aver_Mark, R_))
-        Max_Amount_Index = Pack([(i, i = 1, Size(Group))],[Group%Aver_mark==Max_Amount])
+        Max_Amount_Position = Pack([(i, i = 1, Size(Group))],[Group%Aver_mark==Max_Amount])
     end function Sort_class_list
  
     pure subroutine Sub_Aver_Mark(Group,Aver_Mark)
